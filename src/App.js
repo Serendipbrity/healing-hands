44403c9053a92f8fca4397851b728c2eb84f735f
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-04-16 18:17:47 -0400

    url paths

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/healing-hands">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home/>} />
