5a0315a7fc098332abed0d2afa9bfef33ccff3c4
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-04-21 17:57:49 -0400

    close nav bar when link clicked

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 import icon from '../assets/images/icon.svg';
 
 export default function NavBar() {
+  const navbarRef = useRef(null);
+
+  function handleLinkClick() {
+    navbarRef.current.collapse();
+  }
   return (
-    <Navbar collapseOnSelect expand="lg" className='nfBar' variant="light">
+    <Navbar ref={navbarRef} collapseOnSelect expand="lg" className='nfBar' variant="light">
       <Container>
         <Nav.Link className='logoImgContainer' id='logoImgContainer'><img as={Link} to="/" className='logoImg' src={logo}></img>
         </Nav.Link>
@@ -22,13 +28,17 @@ export default function NavBar() {
         
 
         {/* hamburger */}
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" id='hamburger'/>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" id='hamburger' />
+        
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end hamburger" >
           <Nav basename="/healing-hands">
-            <Nav.Link as={Link} to="/about" className='navLinkChild'>About</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className='navLinkChild'>Contact</Nav.Link>
-            <Nav.Link as={Link} to="/services" className='navLinkChild'>Services</Nav.Link>
-            <Nav.Link as={Link} to="/booknow" className='navLinkChild' href="" onClick={() => window.Calendly.initPopupWidget({url: 'https://calendly.com/bcourt95'})} id='booknowNav'>Book Now</Nav.Link>
+            <Nav.Link onClick={handleLinkClick} as={Link} to="/about" className='navLinkChild'>About</Nav.Link>
+            <Nav.Link onClick={handleLinkClick} as={Link} to="/contact" className='navLinkChild'>Contact</Nav.Link>
+            <Nav.Link onClick={handleLinkClick} as={Link} to="/services" className='navLinkChild'>Services</Nav.Link>
+            <Nav.Link as={Link} to="/booknow" className='navLinkChild' href="" onClick={(event) => {
+              handleLinkClick();
+              window.Calendly.initPopupWidget({ url: 'https://calendly.com/bcourt95' });
+            }} id='booknowNav'>Book Now</Nav.Link>
             {/* <Nav.Link as={Link} to="/signUp" className='navLinkChild'>Sign Up</Nav.Link>
             <Nav.Link as={Link} to="/signIn" className='navLinkChild'>Sign In</Nav.Link> */}
           </Nav>
