00d5b4f715b7b43218e6e65fbe760993cb96ce16
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-04-21 19:09:25 -0400

    fixed nav collapse issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,44 +3,39 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
-import { useRef } from 'react';
-import icon from '../assets/images/icon.svg';
+import { useState } from 'react';
 
 export default function NavBar() {
-  const navbarRef = useRef(null);
+  const [expanded, setExpanded] = useState(false);
 
   function handleLinkClick() {
-    navbarRef.current.collapse();
+    setExpanded(false);
   }
+
   return (
-    <Navbar ref={navbarRef} collapseOnSelect expand="lg" className='nfBar' variant="light">
+    <Navbar
+      expanded={expanded}
+      onToggle={setExpanded}
+      collapseOnSelect
+      expand="lg"
+      className='nfBar'
+      variant="light"
+    >
       <Container>
-        <Nav.Link className='logoImgContainer' id='logoImgContainer'><img as={Link} to="/" className='logoImg' src={logo}></img>
-        </Nav.Link>
-
-        {/* ------- text version of logo ---------- */}
-        {/* <Nav.Link className='logoImgContainer navHH' id='logoImg'>
-        <img as={Link} to="/" className='logoImg navHH' src={icon}></img>
-                <p id="title" className='navHH'>Healing Hands</p> 
-          <p id='tagline' className='navHH'>Massage Therapy</p>
-          </Nav.Link> */}
-        {/* ----------------------------------------- */}
-        
-
-        {/* hamburger */}
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" id='hamburger' />
-        
-        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end hamburger" >
-          <Nav>
-            <Nav.Link onClick={handleLinkClick} as={Link} to="/about" className='navLinkChild'>About</Nav.Link>
-            <Nav.Link onClick={handleLinkClick} as={Link} to="/contact" className='navLinkChild'>Contact</Nav.Link>
-            <Nav.Link onClick={handleLinkClick} as={Link} to="/services" className='navLinkChild'>Services</Nav.Link>
-            <Nav.Link as={Link} to="/booknow" className='navLinkChild' href="" onClick={(event) => {
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/" className='logoImgContainer'><img className='logoImg' src={logo} alt="Logo" /></Nav.Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className='hamburger' />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/about" className='navLinkChild' onClick={handleLinkClick}>About</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className='navLinkChild' onClick={handleLinkClick}>Contact</Nav.Link>
+            <Nav.Link as={Link} to="/services" className='navLinkChild' onClick={handleLinkClick}>Services</Nav.Link>
+            <Nav.Link as={Link} to="/bookNow" className='navLinkChild' id='booknowNav' onClick={(event) => {
+              event.preventDefault();
               handleLinkClick();
               window.Calendly.initPopupWidget({ url: 'https://calendly.com/bcourt95' });
-            }} id='booknowNav'>Book Now</Nav.Link>
-            {/* <Nav.Link as={Link} to="/signUp" className='navLinkChild'>Sign Up</Nav.Link>
-            <Nav.Link as={Link} to="/signIn" className='navLinkChild'>Sign In</Nav.Link> */}
+            }}>Book Now</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
