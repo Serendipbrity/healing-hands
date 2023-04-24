80f36945d41df082d9022b3b3cceb5b4a4d63c7f
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-04-24 13:31:50 -0400

    larger screens about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,11 @@ import pinkLotus from '../assets/images/pinkLotus1.jpg';
 const About = () => {
   return (
     <div id='aboutContainer'>
-      <h1 className='about'>About Us</h1>
+      <h1 className='about' id='aboutUs'>About Us</h1>
+      <div id='imgAndTextContainer'>
       <img id='famImg' className='about' src={fam}></img>
-      <p className='about aboutText' id='aboutText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor.</p>
+        <p className='about aboutText' id='aboutText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor.</p>
+        </div>
     </div>
   );
 };
