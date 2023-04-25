c4142eb2cc12c0c5cc46b65bc624fbd758998690
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-04-25 14:05:01 -0400

    screen sizing for larger screens and got rid of excess bottom on width 414px

--- src/pages/services.js
+++ src/pages/services.js
@@ -5,13 +5,14 @@ const Services = () => {
     <div id='services'
     >
    
-      <h1 >Services & Rates</h1>
+      <h1 id='servicesAndRates'>Services & Rates</h1>
          <div id='serviceWrap'>
-      <h2 className='text'>Massage</h2>
-      <h3 className='text'>60 minutes $75</h3>
-      <h3 className='text'>90 minutes $105</h3>
+      <h2 id='massage' className='text'>Massage</h2>
+      <h3 className='text mins'>60 minutes $75</h3>
+      <h3 className='text mins'>90 minutes $105</h3>
       </div>
-      <button id='sBtn' href="" onClick={() => window.Calendly.initPopupWidget({url: 'https://calendly.com/bcourt95'})}>Book Now</button>
+        <button id='sBtn' href="" onClick={() => window.Calendly.initPopupWidget({ url: 'https://calendly.com/bcourt95' })}>Book Now</button>
+      
       </div>
   );
 };
